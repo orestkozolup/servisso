8fa90969a959efd7e19e437d2c7776d247165123
Orest <kozolup.yakiv@gmail.com>
2023-06-26 12:09:39 +0200

    Basic drawer

--- src/app/app/layout.js
+++ src/app/app/layout.js
@@ -1,6 +1,16 @@
+"use client";
+
+import Drawer from "@mui/material/Drawer";
+
 const AppLayout = ({ children }) => (
   <div>
-    <h2>Left Nav</h2>
+    <Drawer anchor="left" open hideBackdrop>
+      <h2>Servisso</h2>
+      <p>Admin panel</p>
+      <h4>Calendar</h4>
+      <h4>Manage your garage</h4>
+      <h4>Service requests</h4>
+    </Drawer>
     {children}
   </div>
 );
